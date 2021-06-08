6929ce0e1576a6085f8c7352946ca62c36432de8
Viktor Danov <orangeleaf12@gmail.com>
2021-06-08 19:41:24 +0300

    implement log in in auth service

--- server/src/service/authService.ts
+++ server/src/service/authService.ts
@@ -1,8 +1,25 @@
 import { Repository } from '../entities/repository'
+import bcrypt from 'bcrypt'
+import { JWTUtil } from '../jwt'
+
+type APIResponse<T> = Promise<[T, Error | null]>
 
 export class AuthService {
   private repository: Repository
-  constructor(repository: Repository) {
+  private jwtSecret: string
+
+  constructor(repository: Repository, jwtSecret: string) {
     this.repository = repository
+    this.jwtSecret = jwtSecret
+  }
+
+  async login(email: string, password: string): APIResponse<string> {
+    const account = await this.repository.getAccountByEmail(email)
+
+    const isValid = bcrypt.compareSync(password, account.passwordHash)
+    if (isValid) {
+      return [JWTUtil.signJWT(this.jwtSecret), null]
+    }
+    return ['', new Error('Unauthorized')]
   }
 }
