4a16c9958efae36f85aa07add8cbc4185ae08673
Viktor Danov <orangeleaf12@gmail.com>
2021-06-09 02:24:47 +0300

    render user data from token

--- client/src/pages/StudentPanel.tsx
+++ client/src/pages/StudentPanel.tsx
@@ -1,20 +1,36 @@
 import styles from '@/styles/pages/studentPanel.module.scss'
-import React from 'react'
+import React, { useMemo } from 'react'
 import c from 'classnames'
 import { Logo } from '@/components/logo'
 import { PersonBadge } from '@/components/personBadge'
 import { Menu } from '@/components/menu'
+import { useAuth } from '@/hooks/useAuth'
+import { Switch } from 'react-router'
+
+type UserInfo = {
+  name: string
+  email: string
+  accountType: 'admin' | 'student'
+}
 
 export const StudentPanel: React.FunctionComponent = () => {
+  const { decodedToken } = useAuth()
+
+  const userInfo = useMemo<UserInfo>(() => {
+    const subObj = decodedToken?.sub as any
+
+    return { name: subObj.name ?? '', email: subObj.email ?? '', accountType: 'student' }
+  }, [decodedToken])
   return (
     <div className={c(styles.studentPanel)}>
       <header>
         <Logo />
         <div className={styles.right}>
           <Menu className={styles.menu} />
-          <PersonBadge name="tewt" mode="profile" />
+          <PersonBadge name={userInfo.name} email={userInfo.email} mode="profile" />
         </div>
       </header>
+      <Switch></Switch>
     </div>
   )
 }
