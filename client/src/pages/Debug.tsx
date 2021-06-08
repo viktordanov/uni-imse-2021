0c5e36186a3d70fa775de614c8a16fa2303dd01b
Viktor Danov <orangeleaf12@gmail.com>
2021-06-08 17:18:41 +0300

    implement person badges

--- client/src/pages/Debug.tsx
+++ client/src/pages/Debug.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/pages/debug.module.scss'
 import React from 'react'
 import c from 'classnames'
+import { PersonBadge } from '@/components/personBadge'
 
 export interface DebugProps {
   className?: string
@@ -10,7 +11,19 @@ export interface DebugProps {
 export const Debug: React.FunctionComponent<DebugProps> = ({ className, onClick }: DebugProps) => {
   return (
     <div className={c(styles.debug, className)} onClick={onClick}>
-      <p></p>
+      <PersonBadge name="Thomas Angeland" mode="full" />
+      <PersonBadge name="Thomas eland" mode="full" />
+      <PersonBadge name="Phoebe Buffet" mode="full" />
+      <PersonBadge name="Matt LeBlanc" mode="full" />
+      <PersonBadge name="Matt LBlanc" mode="full" />
+      <PersonBadge name="Matt LeBzxca" mode="full" />
+      <PersonBadge name="Matt LeBzxca" mode="full" />
+      <PersonBadge name="Thomas Angeland" mode="compact" />
+      <PersonBadge name="Phoebe Buffet" mode="compact" />
+      <PersonBadge name="Matt LeBlanc" mode="compact" />
+      <PersonBadge name="Thomas Angeland" mode="profile" />
+      <PersonBadge name="Phoebe Buffet" mode="profile" />
+      <PersonBadge name="Matt LeBlanc" mode="profile" />
     </div>
   )
 }
