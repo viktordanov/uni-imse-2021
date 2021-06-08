5cc7b9ebd1cadd6b9795c219a9f6c95043aaf773
Viktor Danov <orangeleaf12@gmail.com>
2021-06-09 01:20:45 +0300

    add jwt payload

--- server/src/jwt.ts
+++ server/src/jwt.ts
@@ -1,8 +1,12 @@
 import * as jwt from 'jsonwebtoken'
-import { AccountType } from './entities/entities'
+import { Account } from './entities/entities'
 
-function signJWT(account: AccountType, jwtSecret: string): string {
-  return jwt.sign({ slkdfjslkd: '' }, jwtSecret, { expiresIn: '24h' })
+function signJWT(account: Account, jwtSecret: string): string {
+  return jwt.sign(
+    { sub: { name: account.name, email: account.email, id: account.id, type: account.accountType } },
+    jwtSecret,
+    { expiresIn: '24h' }
+  )
 }
 
 export const JWTUtil = { signJWT }
