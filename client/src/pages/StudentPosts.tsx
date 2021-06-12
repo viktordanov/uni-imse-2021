32d6b0ee1ed0ac9e5c8cd162b4796d6ea9fe3285
Viktor Danov <orangeleaf12@gmail.com>
2021-06-12 19:37:18 +0300

    implement posts rendering page

--- client/src/pages/StudentPosts.tsx
+++ client/src/pages/StudentPosts.tsx
@@ -1,63 +1,186 @@
-import { APIEndpoints } from '@/api'
+import { APIEndpoints, makeRequest } from '@/api'
+import { FormInput } from '@/components/formInput'
 import { IconButton } from '@/components/iconButton'
-import { PageCard } from '@/components/pageCard'
+import { Modal } from '@/components/modal'
 import { Placeholder } from '@/components/placeholder'
+import { PostCard } from '@/components/postCard'
+import { NotificationType, useNotifications } from '@/context/notifierContext'
+import { useAuth } from '@/hooks/useAuth'
 import { useRequest } from '@/hooks/useRequest'
 import styles from '@/styles/pages/studentPages.module.scss'
-import { Page, Student } from '@/types'
+import modalStyles from '@/styles/components/modal.module.scss'
+import postPageStyles from '@/styles/pages/studentPosts.module.scss'
+import { Post, Student } from '@/types'
 import c from 'classnames'
-import React from 'react'
-import { ChevronLeft } from 'react-feather'
+import React, { useCallback, useMemo, useRef, useState } from 'react'
+import { ChevronLeft, Plus } from 'react-feather'
 import { useHistory, useParams } from 'react-router-dom'
+import { Button } from '@/components/button'
+import { FormTextArea } from '@/components/formTextArea'
 
-export interface StudentPagesProps {
+export interface StudentPostsProps {
   className?: string
   onClick?: (e: React.MouseEvent<HTMLDivElement>) => void
 }
 
-export const StudentPage: React.FunctionComponent<StudentPagesProps> = ({ className, onClick }: StudentPagesProps) => {
-  const { studentEmail } = useParams<{ studentEmail: string }>()
+export const StudentPosts: React.FunctionComponent<StudentPostsProps> = ({ className, onClick }: StudentPostsProps) => {
+  const { token, decodedToken } = useAuth()
+  const { pushNotification } = useNotifications()
+  const { studentEmail, pageTitle } = useParams<{ studentEmail: string; pageTitle: string }>()
   const { goBack } = useHistory()
   const [studentInfo] = useRequest<Student>(
     { email: '', name: '', university: '' },
     APIEndpoints.getStudentInfo(decodeURI(studentEmail))
   )
-  const [pages] = useRequest<Page[]>([], APIEndpoints.getPagesOfStudent(decodeURI(studentEmail)))
+  const [posts, refreshPosts] = useRequest<Post[]>(
+    [],
+    APIEndpoints.getPostsOfStudent(decodeURI(studentEmail), decodeURI(pageTitle)),
+    {},
+    (data: any): Post => {
+      return data.map((d: any) => {
+        return {
+          ownerName: d.ownerName,
+          pageTitle: d.pageTitle,
+          title: d.title,
+          content: d.content,
+          dateCreated: new Date(d.dateCreated)
+        }
+      })
+    }
+  )
+
+  const isMe = useMemo(() => (decodedToken?.sub as any)?.email === decodeURI(studentEmail), [])
+  const who = useMemo(() => {
+    const isMe = (decodedToken?.sub as any)?.email === decodeURI(studentEmail)
+    return isMe ? (decodedToken?.sub as any)?.name : studentInfo.name
+  }, [studentInfo, decodedToken, studentEmail])
+
+  // Modal
+  const [modalIsOpen, setIsOpen] = React.useState(false)
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  const titleRef = useRef<HTMLInputElement>(null)
+  const contentRef = useRef<HTMLTextAreaElement>(null)
+
+  const [postTitle, setPostTitle] = useState('')
+  const [content, setContent] = useState('')
+  const handleNewPost = useCallback(() => {
+    if (!titleRef.current?.validity.valid) {
+      pushNotification(
+        NotificationType.WARNING,
+        'Title invalid',
+        'Please ensure your title is longer than 3 character',
+        3500
+      )
+      return
+    }
+    if (!contentRef.current?.validity.valid) {
+      pushNotification(
+        NotificationType.WARNING,
+        'Content invalid',
+        'Please ensure your post is longer than 3 character and shorter than 300',
+        3500
+      )
+      return
+    }
+
+    makeRequest(APIEndpoints.newPost(pageTitle), 'post', { title: postTitle, content }, token ?? '').then(res => {
+      if (res.ok) {
+        closeModal()
+        pushNotification(NotificationType.SUCCESS, 'Success', 'Successfully added new post', 2000)
+        refreshPosts()
+      } else {
+        pushNotification(NotificationType.ERROR, 'Error', 'Unknown error occurred ' + res.statusText, 2000)
+      }
+    })
+  }, [postTitle, content, token, pageTitle])
 
   return (
     <div className={c(styles.studentPages, className)} onClick={onClick}>
       <div className={styles.pages}>
-        <div className={styles.pagesWrapper}>
-          {pages.length > 0 && (
-            <>
-              <div className={styles.header}>
-                <h1>
-                  {studentInfo.name}'s pages ({pages.length})
-                </h1>
-                <IconButton Icon={ChevronLeft} onClick={goBack}>
-                  Back
+        <div className={c(postPageStyles.wrapper)}>
+          <div className={c(styles.header, postPageStyles.header)}>
+            <h1>
+              {who}
+              <span>/</span>
+              {decodeURI(pageTitle)}
+            </h1>
+            <div>
+              <IconButton className={postPageStyles.button} Icon={ChevronLeft} onClick={goBack}>
+                Back
+              </IconButton>
+              {isMe && (
+                <IconButton className={postPageStyles.button} Icon={Plus} onClick={openModal}>
+                  Post
                 </IconButton>
-              </div>
-              {pages.map((page, index) => {
-                return (
-                  <PageCard
-                    className={styles.pageCard}
-                    pageTitle={page.title}
-                    description={page.description}
-                    postCount={page.postCount}
-                    key={index}
-                  />
-                )
-              })}
-            </>
+              )}
+            </div>
+          </div>
+          {posts.length > 0 && (
+            <div className={postPageStyles.postsWrapper}>
+              {posts
+                .sort((a, b) => b.dateCreated.getTime() - a.dateCreated.getTime())
+                .map((post, index) => {
+                  return (
+                    <PostCard
+                      className={postPageStyles.postCard}
+                      pageTitle={pageTitle}
+                      title={post.title}
+                      dateCreated={post.dateCreated}
+                      content={post.content}
+                      ownerName={who}
+                      key={index}
+                    />
+                  )
+                })}
+            </div>
           )}
-          {pages.length === 0 && (
+          {posts.length === 0 && (
             <Placeholder>
-              <p>{studentInfo.name} doesn't have any pages yet.</p>
+              <p>
+                {isMe ? "You haven't" : studentInfo.name + " hasn't"} posted anything on page {decodeURI(pageTitle)}{' '}
+                yet.
+              </p>
             </Placeholder>
           )}
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        overlayClassName={modalStyles.modalOverlay}
+        title="New post"
+      >
+        <FormInput
+          ref={titleRef}
+          className={styles.formInput}
+          value={postTitle}
+          required
+          minLength={3}
+          maxLength={100}
+          onChange={e => setPostTitle(e.currentTarget.value)}
+          label={'title'}
+        />
+        <FormTextArea
+          ref={contentRef}
+          className={styles.formInput}
+          value={content}
+          required
+          minLength={3}
+          maxLength={300}
+          onChange={e => setContent(e.currentTarget.value)}
+          label={'content'}
+        />
+        <Button className={styles.right} onClick={handleNewPost}>
+          Post
+        </Button>
+      </Modal>
     </div>
   )
 }
