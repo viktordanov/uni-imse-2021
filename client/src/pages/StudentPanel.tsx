8980b1fc3a74839a7af699eabf0cc1bd09924390
Viktor Danov <orangeleaf12@gmail.com>
2021-06-12 14:02:00 +0300

    rename files

--- client/src/pages/StudentPanel.tsx
+++ client/src/pages/StudentPanel.tsx
@@ -8,7 +8,7 @@ import { useAuth } from '@/hooks/useAuth'
 import { Route, Switch } from 'react-router'
 import { StudentHome } from './StudentHome'
 import { StudentFollowing } from './StudentFollowing'
-import { StudentPages } from './StudentPage'
+import { StudentPages } from './StudentPages'
 
 type UserInfo = {
   name: string
