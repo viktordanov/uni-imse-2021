c3eab7183bde91bd607bedcabc6f869a278f7578
raphala <22345578+raphala@users.noreply.github.com>
2021-06-13 12:31:20 +0200

    refactor sql filling

--- server/src/sqlFilling/dbFillingSQL.ts
+++ server/src/sqlFilling/dbFillingSQL.ts
@@ -6,23 +6,30 @@ import { getPage, getPost, getStudents } from './mockData'
 
 function insertData(repository: Repository): void {
   getStudents().then(students => {
-    console.log(students)
+    // console.log(students)
     getPage().then(pages => {
+      // console.log(pages)
       getPost().then(posts => {
         students.forEach(student => {
           repository.addStudent(student).then(() => {
-            pages.forEach(page => {
-              repository.addPage(student.id, page).then(() => {
-                posts.forEach(post => {
-                  repository.addPost(student.id, page.title, post)
-                })
+            const shuffledPages = pages.sort((a, b) => 0.5 - Math.random())
+            for (let i = 0; i < randomNumber(1, 4); i++) {
+              repository.addPage(student.id, shuffledPages[i]).then(() => {
+                const shufflePosts = posts.sort((a, b) => 0.5 - Math.random())
+                for (let i = 0; i < randomNumber(1, 4); i++) {
+                  repository.addPost(student.id, shuffledPages[i].title, shufflePosts[i])
+                }
               })
-            })
+            }
           })
         })
       })
     })
   })
+}
+
+function randomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * max) + min
 }
 
 function insertStudents(repository: Repository, students: Student[]) {
